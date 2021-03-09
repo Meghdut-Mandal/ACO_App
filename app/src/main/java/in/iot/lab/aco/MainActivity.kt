fb4ca446a0307ee9050b3324971a1afc20a1e5a2
Anirban Das <54911675+anirban-das52134@users.noreply.github.com>
2021-03-09 19:54:17 +0530

    Service added to check amount of battery on device.

--- app/src/main/java/in/iot/lab/aco/MainActivity.kt
+++ app/src/main/java/in/iot/lab/aco/MainActivity.kt
@@ -1,11 +1,40 @@
 package `in`.iot.lab.aco
 
-import androidx.appcompat.app.AppCompatActivity
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.content.*
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
+
+private const val TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
+    lateinit var br: BroadcastReceiver
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btn.setOnClickListener {
+            actionOnService(Actions.START)
+        }
+        stopBtn.setOnClickListener {
+            actionOnService(Actions.STOP)
+        }
+    }
+
+    private fun actionOnService(action: Actions) {
+        val mIntent = Intent(this, ServiceStatusUpdate::class.java)
+        val txt:String = txt.text.toString()
+        mIntent.putExtra("val", txt)
+        mIntent.also {
+            it.action = action.name
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                startForegroundService(it)
+                return
+            }
+            startService(it)
+        }
     }
 }
